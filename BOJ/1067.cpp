e511f4b3522285c97ec5e7934315c1076b915700
range0224 <79440246+range0224@users.noreply.github.com>
2021-06-03 11:45:12 +0900

    1067

---
+++ BOJ/1067.cpp
@@ -0,0 +1,99 @@
+#define _USE_MATH_DEFINES
+#include<iostream>
+#include<string>
+#include<vector>
+#include<complex>
+#include<algorithm>
+using namespace std;
+
+const double pi = acos(-1);
+typedef complex<double> cpx;
+typedef vector<cpx> vec;
+
+void fft(vec& a, bool inv) {
+	int n = a.size();
+	for (int i = 1, j = 0; i < n; i++) {
+		int bit = n >> 1;
+		for (; bit <= j; bit >>= 1) j -= bit;
+		j += bit;
+		if (i < j) swap(a[i], a[j]);
+	}
+	for (int len = 2; len <= n; len <<= 1) {
+		double ang = 2 * M_PI / len;
+		if (inv) ang = -ang;
+		cpx w(cos(ang), sin(ang));
+		for (int i = 0; i < n; i += len) {
+			cpx wp(1, 0);
+			for (int j = 0; j < len / 2; j++) {
+				cpx u = a[i + j], v = a[i + j + len / 2] * wp;
+				a[i + j] = u + v;
+				a[i + j + len / 2] = u - v;
+				wp *= w;
+			}
+		}
+	}
+	if (inv) {
+		for (int i = 0; i < n; i++) {
+			a[i] /= n;
+			a[i] = cpx(round(a[i].real()), round(a[i].imag())); //result is integer
+		}
+	}
+}
+
+vector<long long> multiply(vector<long long>& a, vector<long long>& b)
+{
+	vec aa(a.begin(), a.end()), bb(b.begin(), b.end());
+	int n = 1; while (n <= max(a.size(), b.size())) n <<= 1;
+	aa.resize(n), bb.resize(n);
+	fft(aa, 0), fft(bb, 0);
+	for (int i = 0; i < n; i++) aa[i] *= bb[i];
+	fft(aa, 1);
+	vector<long long> ret(n);
+	for (int i = 0; i < n; i++) {
+		ret[i] = round(aa[i].real());
+	}
+	//while(ret.size() > 10 && ret.back() == 0) ret.pop_back();
+	return ret;
+}
+
+int main()
+{
+	long long n;
+	vector<long long> a;
+	vector<long long> b;
+	vector<long long> v;
+	int i, j;
+	int size = 1;
+	long long answer;
+
+	cin >> n;
+
+	while (size < 2 * n) size *= 2;
+
+	a.resize(size, 0);
+	b.resize(size, 0);
+
+	for (i = 0; i < n; i++)
+	{
+		cin >> a[i];
+	}
+	for (i = n - 1; i >= 0; i--)
+	{
+		cin >> b[i];
+	}
+
+	for (i = n; i < 2 * n; i++)
+	{
+		a[i] = a[i - n];
+	}
+
+	v = multiply(a, b);
+
+	answer = 0;
+	for (i = n - 1; i < v.size(); i++)
+	{
+		answer = max(answer, v[i]);
+	}
+
+	cout << answer;
+}
